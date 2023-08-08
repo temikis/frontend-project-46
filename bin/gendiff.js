f61912a63207801c55d73b69473d5850d29babd0
temikis <temikis@mail.ru>
2023-08-08 13:57:56 +0200

    add format selection

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-import genDiff from '../src/gendiff.js';
+// import genDiff from '../src/gendiff.js';
 
 program
   .name('gendiff')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log('First path:', filepath1);
+    console.log('Secons path:', filepath2);
+    // console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
-
-console.log(genDiff(filepath1, filepath2));
